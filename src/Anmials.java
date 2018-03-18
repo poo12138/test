b226f0f1af1b734197dc207c0306ad95b6a56b46
poo12138 <347547721@qq.com>
2018-03-18 23:12:49 +0800

    Initial commit

---
+++ src/Anmials.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Anmials {
+        static Random rand = new Random();
+        static int pRand(int mod) {
+            return Math.abs(rand.nextInt()) % mod + 1;
+        }
+        public static void main(String[] args) {
+            int[] a;
+            a = new int[pRand(20)];
+            prt("length of a = " + a.length);
+            for(int i = 0; i < a.length; i++){
+                a[i]++;
+                prt("a[" + i + "] = " + a[i]);}
+        }
+        static void prt(String s) {
+            System.out.println(s);
+        }
+    }
+
+
